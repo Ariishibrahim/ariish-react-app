d3991af761873c2b46845c5ebdbc283a730105f4
Ariish Ibrahim <katra_abdi@yahoo.com>
2025-05-16 17:11:52 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+  import './App.css';
+  import Greetings from "./components/Greetings";
+  import Header from './components/Header';
+
+
+function App() {
+
+  return (
+    <>
+    <Greetings name= "Ariish" surname = "Dahir" />
+    <Header />
+    
+    </>
+  );
+
+}
+
+export default App;
